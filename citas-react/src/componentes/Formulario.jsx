dbffa25420265f5877837b9084052e6013539810
franzcito <gavino_10@hotmail.es>
2022-07-14 22:19:10 -0500

    usestate

--- citas-react/src/componentes/Formulario.jsx
+++ citas-react/src/componentes/Formulario.jsx
@@ -6,16 +6,27 @@ const Formulario = () => {
   const [email, setemail] = useState('');
   const [alta, setalta] = useState('');
   const [sintomas, setsintomas] = useState('');
+
+  const [error, seterror] = useState(false);
   
   const handlesubmit=(a)=>{
     a.preventDefault();
-    console.log('enviando formulario');
+    if ([nombre,propietario,email,alta,sintomas].includes('')) {
+      seterror(true)
+      return;
+    }
+      seterror(false);
   }
   return (
     <div className=" w-1/2">
       <h1 className=' font-black text-3xl text-center'>Seguimientos Paicientes</h1>
       <p className=' text-lg mt-5 text-center mb-10'> Añada pacientes y {''} <span className=' text-indigo-600 font-bold '>Administralos</span></p>
       <form onSubmit={handlesubmit} action="" className=' bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+        {error && (
+          <div className=' bg-red-800 text-white text-center p-3 font-bold mb-3 rounded-md'>
+            <p>TODOS LOS CAMPOS SON OBLIGATORIOS</p>
+          </div>
+        )}
           <div className="mb-5">
             <label htmlFor="mascota" className=' block text-gray-700 uppercase font-bold'>Mascota</label>
             <input type="text" id='mascota' value={nombre} onChange={(e)=>setnombre(e.target.value)} placeholder='nombre de la mascota' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
