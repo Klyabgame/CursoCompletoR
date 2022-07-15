f2cbb653079fd323644b2e44e61169b250314e1e
franzcito <gavino_10@hotmail.es>
2022-07-14 21:39:05 -0500

    commit emegercia

--- citas-react/src/componentes/Formulario.jsx
+++ citas-react/src/componentes/Formulario.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from 'react'
 
 const Formulario = () => {
-  const [nombre, setNombre] = useState('');
-  setNombre('hook');
-  console.log(nombre);
-
+  const [nombre, setnombre] = useState('');
+  const [propietario, setpropietario] = useState('');
+  const [email, setemail] = useState('');
+  const [alta, setalta] = useState('');
+  const [sintomas, setsintomas] = useState('');
+  
+  const handlesubmit=(a)=>{
+    a.preventDefault();
+    console.log('enviando formulario');
+  }
   return (
     <div className=" w-1/2">
       <h1 className=' font-black text-3xl text-center'>Seguimientos Paicientes</h1>
       <p className=' text-lg mt-5 text-center mb-10'> Añada pacientes y {''} <span className=' text-indigo-600 font-bold '>Administralos</span></p>
-      <form action="" className=' bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+      <form onSubmit={handlesubmit} action="" className=' bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
           <div className="mb-5">
             <label htmlFor="mascota" className=' block text-gray-700 uppercase font-bold'>Mascota</label>
-            <input type="text" id='mascota' placeholder='nombre de la mascota' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            <input type="text" id='mascota' value={nombre} onChange={(e)=>setnombre(e.target.value)} placeholder='nombre de la mascota' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
           </div>
           <div className="mb-5">
             <label htmlFor="propietario" className=' block text-gray-700 uppercase font-bold'>Propietario</label>
-            <input type="text" id='propietario' placeholder='nombre del propietario' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            <input type="text" id='propietario' value={propietario} onChange={(e)=>setpropietario(e.target.value)}  placeholder='nombre del propietario' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
           </div>
           <div className="mb-5">
             <label htmlFor="email" className=' block text-gray-700 uppercase font-bold'>Email</label>
-            <input type="email" id='email' placeholder='Ingrese su Correo' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            <input type="email" id='email' value={email} onChange={(e)=>setemail(e.target.value)}  placeholder='Ingrese su Correo' className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
           </div>
           <div className="mb-5">
             <label htmlFor="alta" className=' block text-gray-700 uppercase font-bold'>ALTA</label>
-            <input type="date" id='alta'  className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            <input type="date" id='alta'  value={alta} onChange={(e)=>setalta(e.target.value)}  className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
           </div>
           <div className="mb-5">
             <label htmlFor="sintomas" className=' block text-gray-700 uppercase font-bold'>sintomas</label>
-            <textarea id="sintomas" className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder='Describe los sintomas'></textarea>
+            <textarea id="sintomas" value={sintomas} onChange={(e)=>setsintomas(e.target.value)}  className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder='Describe los sintomas'></textarea>
           </div>
           <input type="submit"  className=" bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" value="agregar paciente"/>
       </form>
